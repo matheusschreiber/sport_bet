c3d3ce18232824834da1adfe8875ed3f5793c516
Matheus Schreiber <mt.schreiber01@gmail.com>
2022-03-27 19:18:46 -0300

    Adding vercel front end deploy link to the cors list

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ const router = require('./routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({client:'https://sport-bet.vercel.app/'}));
 app.use(router);
 
 app.use((err, req, res, next) =>{ 
