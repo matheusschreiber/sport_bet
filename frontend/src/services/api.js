82067b1fd5389a2d6e95d8183d85a990b69bde88
Matheus <mt.schreiber01@gmail.com>
2023-01-29 23:21:39 -0300

    add cyclic url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL:'http://localhost:8080'
-  baseURL:'https://sportbet-production.up.railway.app'
+  // baseURL:'https://sportbet-production.up.railway.app'
+  baseURL:'https://sport-bet.cyclic.app'
 })
 
 export default api;
