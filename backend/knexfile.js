a013aab2aae5cce5c60cfbfb0edd238720acc6dc
Matheus <mt.schreiber01@gmail.com>
2023-01-29 23:17:56 -0300

    add cyclic knexfile path

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,11 +1,13 @@
 // Update with your config settings.
 
+const path = require('path')
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/dev.sqlite3'
+      filename: path.resolve(__dirname, 'src', 'database', 'dev.sqlite3')
     },
     migrations: {
       directory: './src/database/migrations'
